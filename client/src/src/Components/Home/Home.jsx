585589247abea0949faed9530694fe624fbc16ae
ricardo-719 <teacherrickyricardo@gmail.com>
2023-05-19 22:54:06 +0700

    Set home layout & update favicon

--- client/src/src/Components/Home/Home.jsx
+++ client/src/src/Components/Home/Home.jsx
@@ -1,6 +1,30 @@
+import './Home.css'
+
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div id='homeComponent'>
+        <header>HEADER COMPONENT</header>
+        <main>
+            <section id="chessNewsSection">
+                News Section (COMPONENT)
+            </section>
+            <section id="playChessSectionContainer">
+                <div id='quickStartSection'>
+                    Quick Start Section (COMPONENT)
+                </div>
+                <div id='puzzleAndStatsSection'>
+                    <div className='puzzleStatsBtn'>
+                        Puzzle of the day
+                    </div>
+                    <div className='puzzleStatsBtn'>
+                        Stats
+                    </div>
+                </div>
+            </section>
+        </main>
+        <footer>FOOTER COMPONENT</footer>
+    </div>
+    
   )
 }
 export default Home
