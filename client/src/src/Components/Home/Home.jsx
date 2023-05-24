09348d37064012ebdf2e3369cd61a7593d202716
Gensu123 <62147414+Gensu123@users.noreply.github.com>
2023-05-23 22:07:45 -0400

    organized the grid into a different folder

--- client/src/src/Components/Home/Home.jsx
+++ client/src/src/Components/Home/Home.jsx
@@ -2,7 +2,7 @@ import {useEffect, useState} from 'react'
 import './Home.css'
 import {Header} from '../Header/Header'
 import {Articles} from '../Articles/Articles'
-
+import QuickStartSection from './Quick Start Section/QuickStartSection'
 export const Home = () => {
 
     const [article, setArticle] = useState("");
@@ -48,100 +48,7 @@ export const Home = () => {
                 <Articles article={article} isLoading={isLoading} isError={isError} fetchData={fetchData}/>
             </section>
             <section id="playChessSectionContainer">
-            <div id='quickStartSectionContainer'>
-            
-                <div id='quickStartSection'>
-                    Quick Start Section
-                <div>
-                    <div className='quickStartBtn'>
-                    <div class="lobby__app__content lpools">
-
-                    <button>
-                    <div data-id="1+0">
-                    <div class="clock">1+0</div>
-                    <div class="perf">Bullet</div>
-                    </div>
-                    </button>
-
-                    <button>
-                    <div data-id="2+1">
-                    <div class="clock">2+1</div>
-                    <div class="perf">Bullet</div>
-                    </div>
-                    </button>
-
-                    <button>
-                    <div data-id="3+0">
-                    <div class="clock">3+0</div>
-                    <div class="perf">Blitz</div>
-                    </div>
-                    </button>
-
-                    <button>
-                    <div data-id="3+2">
-                    <div class="clock">3+2</div>
-                    <div class="perf">Blitz</div>
-                    </div>
-                    </button>
-                    <button>
-                    <div data-id="5+0">
-                    <div class="clock">5+0</div>
-                    <div class="perf">Blitz</div>
-                    </div>
-                    </button>
-
-                    <button>
-                    <div data-id="5+3">
-                    <div class="clock">5+3</div>
-                    <div class="clock">Blitz</div>
-                    </div>
-                    </button>
-
-                    <button>
-                    <div data-id="10+0">
-                    <div class="clock">10+0</div>
-                    <div class="perf">Rapid</div>
-                    </div>
-                    </button>
-
-                    <button>
-                    <div data-id="10+5">
-                    <div class="clock">10+5</div>
-                    <div class="perf">Rapid</div>
-                    </div>
-                    </button>
-
-                    <button>
-                    <div data-id="15+10">
-                    <div class="clock">15+10</div>
-                    <div class="perf">Rapid</div>
-                    </div>
-                    </button>
-
-                    <button>
-                    <div data-id="30+0">
-                    <div class="clock">30+0</div>
-                    <div class="perf">Classical</div>
-                    </div>
-                    </button>
-
-                    <button>
-                    <div data-id="30+20">
-                    <div class="clock">30+20</div>
-                    <div class="perf">Classical</div>
-                    </div>
-                    </button>
-
-                    <button>
-                    <div class="custom" data-id="custom">Custom</div>
-                    </button>
-                    </div>
-                    
-                </div>
-                </div>
-                </div>
-
-                </div>
+                <QuickStartSection/>
                 <div id='puzzleAndStatsSection'>
                     <div className='puzzleStatsBtn'>
                         Puzzle of the day
