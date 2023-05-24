9f4056b40325b2fe67f9d1da24f7653dfc648544
Gensu123 <62147414+Gensu123@users.noreply.github.com>
2023-05-23 22:15:58 -0400

    organization for the grid

---
+++ client/src/src/Components/Home/Quick Start Section/QuickStartSection.jsx
@@ -0,0 +1,106 @@
+import React from 'react';
+import './Grid.css';
+
+
+
+const QuickStartSection = () => {
+    return (
+<div id='quickStartSectionContainer'>
+            
+                <div id='quickStartSection'>
+                    Quick Start Section
+                <div>
+                    <div className='quickStartBtn'>
+                    <div class="lobby__app__content lpools">
+
+                    <button>
+                    <div data-id="1+0">
+                    <div class="clock">1+0</div>
+                    <div class="perf">Bullet</div>
+                    </div>
+                    </button>
+
+                    <button>
+                    <div data-id="2+1">
+                    <div class="clock">2+1</div>
+                    <div class="perf">Bullet</div>
+                    </div>
+                    </button>
+
+                    <button>
+                    <div data-id="3+0">
+                    <div class="clock">3+0</div>
+                    <div class="perf">Blitz</div>
+                    </div>
+                    </button>
+
+                    <button>
+                    <div data-id="3+2">
+                    <div class="clock">3+2</div>
+                    <div class="perf">Blitz</div>
+                    </div>
+                    </button>
+                    <button>
+                    <div data-id="5+0">
+                    <div class="clock">5+0</div>
+                    <div class="perf">Blitz</div>
+                    </div>
+                    </button>
+
+                    <button>
+                    <div data-id="5+3">
+                    <div class="clock">5+3</div>
+                    <div class="clock">Blitz</div>
+                    </div>
+                    </button>
+
+                    <button>
+                    <div data-id="10+0">
+                    <div class="clock">10+0</div>
+                    <div class="perf">Rapid</div>
+                    </div>
+                    </button>
+
+                    <button>
+                    <div data-id="10+5">
+                    <div class="clock">10+5</div>
+                    <div class="perf">Rapid</div>
+                    </div>
+                    </button>
+
+                    <button>
+                    <div data-id="15+10">
+                    <div class="clock">15+10</div>
+                    <div class="perf">Rapid</div>
+                    </div>
+                    </button>
+
+                    <button>
+                    <div data-id="30+0">
+                    <div class="clock">30+0</div>
+                    <div class="perf">Classical</div>
+                    </div>
+                    </button>
+
+                    <button>
+                    <div data-id="30+20">
+                    <div class="clock">30+20</div>
+                    <div class="perf">Classical</div>
+                    </div>
+                    </button>
+
+                    <button>
+                    <div class="custom" data-id="custom">Custom</div>
+                    </button>
+                    </div>
+                    
+                </div>
+                </div>
+                </div>
+
+                </div>
+    )
+}
+                export default QuickStartSection;
+
+                
